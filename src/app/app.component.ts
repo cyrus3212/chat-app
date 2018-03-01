4587f7c285c65cb0ab2cb4fc4d443df03fcd96a9
cyrus <cyrus@coderepubliq.com>
2018-03-01 22:50:07 +0800

    base code for chat app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+interface Post {
+  title: string;
+  content: string;
+}
+interface PostId extends Post { 
+  id: string; 
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +17,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  postsCol: AngularFirestoreCollection<Post>;
+  posts: any;
+  postDoc: AngularFirestoreDocument<Post>;
+  post: Observable<Post>;
+
+  constructor(private afs: AngularFirestore) {
+
+  }
+
+  ngOnInit() {
+    this.postsCol = this.afs.collection('posts');
+    // this.postsCol = this.afs.collection('posts', ref => ref.where('title', '==', 'coursetro'));
+    // this.posts = this.postsCol.valueChanges();
+
+    this.posts = this.postsCol.snapshotChanges()
+      .map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Post;
+          const id = a.payload.doc.id;
+          return { id, data };
+        });
+      });
+  }
+
+  title:string;
+  content:string;
+
+  addPost() {
+    this.afs.collection('posts').add({'title': this.title, 'content': this.content});
+  }
+
+  getPost(postId) {
+    this.postDoc = this.afs.doc('posts/'+postId);
+    this.post = this.postDoc.valueChanges();
+  }
+
+  deletePost(postId) {
+    this.afs.doc('posts/'+postId).delete();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+var config = {
+    apiKey: "AIzaSyBPf4dpTtMBCUa639F2nzfg56YZp0k2xaM",
+    authDomain: "chat-app-682c6.firebaseapp.com",
+    databaseURL: "https://chat-app-682c6.firebaseio.com",
+    projectId: "chat-app-682c6",
+    storageBucket: "",
+    messagingSenderId: "122477007076"
+  };
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
